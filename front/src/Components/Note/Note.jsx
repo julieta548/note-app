e50032f8187e8434e04d9b2607b3c23f6c681338
julieta548 <julietasosa548@gmail.com>
2023-05-10 17:30:46 -0300

    .

--- front/src/Components/Note/Note.jsx
+++ front/src/Components/Note/Note.jsx
@@ -73,8 +73,11 @@ export const Note = ({ notes, goToArchive, getNotes }) => {
 
   return (
     <div>
+      
       {goToArchive ?
-        <> <div className='container-layout'>
+        <>
+        <h1 className='main-title'>ARCHIVED NOTES</h1>
+         <div className='container-layout'>
           {
             notes.map(note => (
 
@@ -87,7 +90,7 @@ export const Note = ({ notes, goToArchive, getNotes }) => {
                       <div className="card-body">
                         <h5 className="card-title">{note.title}</h5>
                         <p className="card-text">{note.description}</p>
-                        <a href="#" onClick={() => archiveNote(note.id)} className="button btn btn-primary"><i class="bi bi-box-arrow-in-down"></i>{note.archived ? ' Unarchive' : ' Archive'}</a>
+                        <a href="#" onClick={() => archiveNote(note.id)} className="button btn btn-primary">{note.archived ? <><i class="bi bi-box-arrow-up"></i> Unarchive </> : <><i class="bi bi-box-arrow-in-down"></i> Archive </>}</a>
                         <a href="#" onClick={() => deleteNote(note.id)} className="button btn btn-danger"><i class="bi bi-trash"></i> Delete</a>
                         <a href="#" onClick={() => guardarId(note.id)} data-bs-toggle="modal" data-bs-target="#exampleModal2" className="button btn btn-success"><i class="bi bi-pencil-square"></i> Edit</a>
                       </div>
@@ -132,26 +135,54 @@ export const Note = ({ notes, goToArchive, getNotes }) => {
         </div>
         </>
         :
-        <><div className='container-layout'>  {
-          notes.map(note => (
+        <>
+        <h1 className='main-title'>NOTES</h1>
+        
+        <div className='container-layout'>
+          {
+            notes.map(note => (
 
-            <div key={note.id}>
-              {note.archived ? '' :
-                <div className="card" >
-                  <img src="./1.png" className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">{note.title}</h5>
-                    <p className="card-text">{note.description}</p>
-                    <a href="#" onClick={() => archiveNote(note.id)} className="button btn btn-primary"><i class="bi bi-box-arrow-in-down"></i>{note.archived ? ' Unarchive' : ' Archive'}</a>
-                    <a href="#" onClick={() => deleteNote(note.id)} className="button btn btn-danger"><i class="bi bi-trash"></i> Delete</a>
-                    <a href="#" onClick={() => guardarId(note.id)} data-bs-toggle="modal" data-bs-target="#exampleModal2" className="button btn btn-success"><i class="bi bi-pencil-square"></i> Edit</a>
+              <div key={note.id}>
+                {note.archived ? '' :
+                  <div className="card" >
+                    <div className="card-body">
+                      <h5 className="card-title">{note.title}</h5>
+                      <p className="card-text">{note.description}</p>
+                      <a href="#" onClick={() => archiveNote(note.id)} className="button btn btn-primary">{note.archived ? <><i class="bi bi-box-arrow-up"></i> Unarchive </> : <><i class="bi bi-box-arrow-in-down"></i> Archive </>}</a>
+                      <a href="#" onClick={() => deleteNote(note.id)} className="button btn btn-danger"><i class="bi bi-trash"></i> Delete</a>
+                      <a href="#" onClick={() => guardarId(note.id)} data-bs-toggle="modal" data-bs-target="#exampleModal2" className="button btn btn-success"><i class="bi bi-pencil-square"></i> Edit</a>
+                    </div>
                   </div>
+                }
+              </div>
+            ))
+          }
+
+          <div className="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLabel">Edit note</h5>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-              }
+                <div className="modal-body">
+                  <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Title</label>
+                    <input name='title' value={noteEdited.title} onChange={fillEditNote} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                  </div>
+                  <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Content</label>
+                    <input name='description' value={noteEdited.description} onChange={fillEditNote} type="text" className="form-control" id="exampleInputPassword1" />
+                  </div>
+
+                  <button data-bs-dismiss="modal" onClick={() => editNote(noteEdited, id)} type="button" className="btn btn-primary">Edit</button>
+                </div>
+
+              </div>
             </div>
-          ))
-        }
+          </div>
         </div>
+
         </>
       }
     </div>
